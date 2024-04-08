15c97e7c238fa360c8052af9abe5a94cc1f6f583
RituPatel21 <165664195+RituPatel21@users.noreply.github.com>
2024-04-08 17:46:10 +0530

    Add files via upload

---
+++ introduction.c
@@ -0,0 +1,12 @@
+//display information using printf name , birthdate, age ,address.
+
+#include<stdio.h>
+int main()
+{
+	int name,age,birthdate,address;
+	printf("My name is Ritu\n");
+	printf("My birthdate is 21 sept 2001\n");
+	printf("I am 23 years old\n");
+	printf("I live in Himatnagr");
+	return 0;
+}
